5e327fdd12546bcd77dcadd1fe561c8e566bbdc1
Dominic Chuma <chumadominic@gmail.com>
2022-08-27 21:58:44 +0100

    importations from react-router-dom done in index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import Router & other pages that are to be routed to
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
